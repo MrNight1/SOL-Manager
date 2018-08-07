fc6ad41b70e79c0fa62e1face9d49ffbdea38745
CHARMIN-PC\Oscar David <o.d.anota@gmail.com>
2018-08-07 07:56:11 -0500

    batch test. Escritura por lotes

--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -2,24 +2,29 @@ import { Injectable } from '@angular/core';
 import { DbserviceService } from './dbservice.service';
 import { Observable } from 'rxjs';
 import { Payment } from '../models/payment';
+import { AngularFirestore } from '../../../node_modules/angularfire2/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentService {
 
-  constructor(private db: DbserviceService) { }
+  constructor(private db: DbserviceService, private dbBatch: AngularFirestore) { }
 
   getPayments(idSale: string): Observable<Payment[]> {
     return this.db.getAllItems('sales/' + idSale + '/payments');
   }
 
   addPay(idSale: string, payment: Payment) {
-    this.db.addItem('sales/' + idSale + '/payments', {
+    const test = this.dbBatch.firestore.batch();
+    const saleDoc = this.dbBatch.doc('sales/' + idSale);
+    console.log('Type: ', typeof(saleDoc));
+    // test.update(saleDoc., { paidAmount: +payment.amount });
+    /*this.db.addItem('sales/' + idSale + '/payments', {
       amount: +payment.amount,
       date: payment.date,
       desc: payment.desc,
       who: payment.who
-    });
+    });*/
   }
 }
